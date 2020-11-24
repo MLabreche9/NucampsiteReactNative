f4b659d075090f035ceafa00ced11f38d4f386d4
Meghan La Breche <Megavictoria@gmail.com>
2020-11-23 20:37:32 -0800

    Form

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
+import Reservation from './ReservationComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
@@ -118,6 +119,29 @@ const ContactNavigator = createStackNavigator(
     }
 );
 
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView
@@ -165,6 +189,19 @@ const MainNavigator = createDrawerNavigator(
                 )
             }
          },
+         Reservation: { 
+            screen: ReservationNavigator,
+            navigationOptions: {
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='tree'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+         },
         About: { 
             screen: AboutNavigator,
             navigationOptions: {
@@ -192,7 +229,7 @@ const MainNavigator = createDrawerNavigator(
                     />
                 )
             }
-         }
+         },
     },
     {
         drawerBackgroundColor: '#CEC8FF',
